7f2b9f027660ca30d66d2d6c0637d101ac74c637
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-01 21:55:45 +0800

    Menambahkan Modul 1

---
+++ Modul1_JovanGilbertNatamasindah_2310817310002/Python/PRAK109-2310817310002-JovanGilbertNatamasindah.py
@@ -0,0 +1,10 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+YUZHONG = 958730
+PAHLAWAN = 5
+TOTAL = round(YUZHONG / PAHLAWAN)
+
+print("Jumlah pasukan yang dibawa Yu Zhong = ?", YUZHONG)
+print("Jumlah pahlawan = ?", PAHLAWAN)
+print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {TOTAL} pasukan ")
