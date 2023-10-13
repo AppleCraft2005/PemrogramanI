21199c6119566854edf9e6b2a05a2a94b11e4b7b
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-13 19:18:59 +0800

    menghapus Modul2_JovanGilbertNatamasindah_2310817310002 directory

--- Modul2_JovanGilbertNatamasindah_2310817310002/Python/PRAK202-2310817310002-JovanGilbertNatamasindah.py
+++
@@ -1,5 +0,0 @@
-N1 = input("Masukkan Nilai Pertama : ")
-N2 = input("Masukkan Nilai Kedua : ")
-HASIL = float(float(N1) + float(N2))
-
-print(f"Hasil dari penjumlahan nilai pertama \"{N1}\" dan nilai kedua \"{N2}\" adalah \"{HASIL}\" ")
