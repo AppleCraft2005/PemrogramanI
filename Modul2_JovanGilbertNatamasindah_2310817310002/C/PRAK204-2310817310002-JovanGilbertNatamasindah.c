5af53d02d8ddab8a9221fe2390032f9ffe8aa0aa
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-14 09:21:07 +0800

    menmbahkan kembali modul2

---
+++ Modul2_JovanGilbertNatamasindah_2310817310002/C/PRAK204-2310817310002-JovanGilbertNatamasindah.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    float jARI_JARI,TINGGI,VOLUME,LUAS,KELILING;
+
+    scanf("%f %f", &jARI_JARI, &TINGGI);
+    
+    printf("Volume : %.2f\n",VOLUME = 3.14 * jARI_JARI * jARI_JARI * TINGGI);
+    printf("Luas : %.2f\n",LUAS = (2 * 3.14 * jARI_JARI * jARI_JARI) + (2 * 3.14 * jARI_JARI * TINGGI));
+    printf("Keliling : %.2f\n",KELILING = 2 * 3.14 * jARI_JARI);
+
+}
