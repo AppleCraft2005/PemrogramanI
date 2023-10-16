4073270e1629046c7976e1ffcb2cf99cf2b1161c
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-16 21:31:19 +0800

    menambahkan ulang modul 2

---
+++ Modul2_JovanGilbertNatamasindah_2310817310002/C/PRAK202-2310817310002-JovanGilbertNatamasindah.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main () {
+
+float N1 ;
+float N2 ;
+float HASIL ;       //operasi aritmatika diletakan diakhir karena jika diletakkan di baris k-8, maka perhitungan tidak akan jalan karena operasi aritmatika terletak sebelum scanf atau tempat uesr menginputkan angka
+
+printf("Masukkan Nilai Pertama : ");
+scanf("%f", &N1);
+
+printf("Masukkan Nilai Kedua : ");
+scanf("%fg", &N2);
+
+printf("\nHasil dari penjumlahan nilai pertama \"%.2g\" dan nilai kedua \"%.1f\" adalah \"%.2f\" \n", N1,N2,HASIL = N1 + N2 );
+
+}
