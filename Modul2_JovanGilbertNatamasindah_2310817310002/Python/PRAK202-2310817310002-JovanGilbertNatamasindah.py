bd9ff75a21962a8b20dc0c84d420d8d4316475a7
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-16 21:41:00 +0800

    Add files via upload

---
+++ Modul2_JovanGilbertNatamasindah_2310817310002/Python/PRAK202-2310817310002-JovanGilbertNatamasindah.py
@@ -0,0 +1,6 @@
+N1 = input("Masukkan Nilai Pertama : ")
+N2 = input("Masukkan Nilai Kedua : ")
+HASIL = (float(N1) + float(N2))
+
+print(f"Hasil dari penjumlahan nilai pertama \"{N1}\" dan nilai kedua \"{N2}\" adalah \"{HASIL:.2f}\" ")
+
