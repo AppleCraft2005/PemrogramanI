72b852d46dee736af0071d99d5712b468d519089
Jovan Gilbert N <141586230+AppleCraft2005@users.noreply.github.com>
2023-10-16 21:02:37 +0800

    mengahpus lagi Modul2_JovanGilbertNatamasindah_2310817310002 directory

--- Modul2_JovanGilbertNatamasindah_2310817310002/C/PRAK203-2310817310002-JovanGilbertNatamasindah.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main () {
-    float a,b,i,j,x,y,HASIL;
-
-    scanf("%f %f %f %f %f %f", &a, &b, &i, &j, &x, &y);
-    printf("%.3f", HASIL = (a - b) * i / j - (x + y));
-}
